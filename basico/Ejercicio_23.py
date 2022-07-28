f212ddd88ab183d639ff756b25f8b3fa27ca09b2
newetty <ettyguerra@gmail.com>
2022-07-28 11:56:43 +0200

    Comienza Ejercicio 23

---
+++ basico/Ejercicio_23.py
@@ -0,0 +1,17 @@
+""""Ejercicios
+1) Desarrolla el siguiente ejercicios de POO: Alumnos -> Es la clase. init -> Es el método init
+nombre, edad, asignatura y nota son las propiedades Instanciamos.los posibles alumnos (alumno1, alumno2, alumno3..) --> son los "objetos"
+y el.init los inicializa """
+
+class Alumnos:
+def__init__(self, nombre, edad, asignatura, nota):
+    self.nombre = nombre
+    self.edad = edad
+    self.asignatura = asignatura
+    self.nota = nota
+def alumno1(self):
+    print("la edad del cliente seleccionado es: ", self.edad)
+def alumno2(self):
+    print("este es el metodo2")
+def alumno3(self):
+    print("este es el metodo2")

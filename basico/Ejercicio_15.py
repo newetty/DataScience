5cbfbeaf000e6ba5b875f9dc8ceabb3de1452417
newetty <ettyguerra@gmail.com>
2022-09-13 10:45:46 +0200

    No empezado

--- basico/Ejercicio_15.py
+++ basico/Ejercicio_15.py
@@ -1,3 +1,6 @@
+
+import pandas as pd
+
 #1- Crea una lista de nombre "concursantes" que contenga los seguientes valores: "Juan", "Pedro", "Andrea", "Luis",
 #"Lara", "Jose", "Estefania" ##
 
@@ -10,14 +13,14 @@ resultados = [20, 30, 50, 20, 10, 5, 60, 40]
 
 #3- Crea un dicionario con los concursantes y los resultados#
 
-data = {"Concursantes": concursantes; "Resultados" : resultados}
+data = {"Concursantes": concursantes, "Resultados" : resultados}
 
-data2 = dict(zip(concursante, resultados))
+data2 = dict(zip(concursantes, resultados))
 data2
 
 #3 Crea un dataframe vacio y apendiza los concursantes y los resultados mediante el empleo de un bucle for#
 
-df = pd.DataFrame(data, columns= ["Concursante", "Resultados"])
+df = pd.DataFrame(data, columns= ["Concursantes", "Resultados"])
 
 df.nuevo = []
 for elemento in data.items():

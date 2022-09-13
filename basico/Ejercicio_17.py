30210e93ebaca7a9ef8a62926e29202e78e8da3a
newetty <ettyguerra@gmail.com>
2022-09-13 11:32:36 +0200

    Falta ejecutar -ver salida

---
+++ basico/Ejercicio_17.py
@@ -0,0 +1,36 @@
+#Ejercicio 1
+#Crea la clase "matematicas" y dentro de esa clase crea las funciones suma y resta, en dichas funciones nos deberán devolver el resultado
+#de la suma y de la resta, siguiendo los siguientes pasos#:  matematicas:
+
+class Matematicas:
+    
+    def funcion_suma(x):
+        y = x + 17
+        return y 
+    def funcion_resta(x):
+        y = x - 10
+        return y
+    Matematicas.funcion_suma(8)
+
+    Matematicas.funcion_resta(9)
+
+
+#Ejercicio 2
+
+#Crea dos clases una llamala "libros" y otra clase "materias". A la clase libros declara una función con nombre "tipos" y dentro de clase materias declara una función con nombre "asignaturas". A la función tipos retorne el valor name = "Data Science" y la función asignaturas retorne nombre = "Big Data"
+
+
+class libros:
+    def funcion_tipos():
+        name = "Data Science"
+        return (name)
+
+class materias(libros):
+    def funcion_asignatura():
+        nombre = "Big Data"
+        return(nombre)
+
+libros.funcion_tipos()
+
+libros.funcion_asignatura()
+

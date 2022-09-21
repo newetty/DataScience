e39a065d3e245f47861a0f895b0f195a7c1483bc
newetty <ettyguerra@gmail.com>
2022-09-21 19:32:36 +0200

    Terminado

---
+++ basico/Ejercicio_3_POO.py
@@ -0,0 +1,36 @@
+"""Escribir un programa que pregunte al usuario su edad y 
+muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad)."""
+
+from mailbox import NoSuchMailboxError
+
+#desconectar para ejecutar#
+#edad = int(input("Tu edad es?:"))
+#for i in range(edad):
+#    print("Has cumplido" + str(i+1) + "años")
+
+
+"""""Desarrolla el siguiente ejercicios de POO:
+Alumnos -> Es la clase.
+init -> Es el método init
+nombre, edad, asignatura y nota son las propiedades
+Instanciamos..
+los posibles alumnos (alumno1, alumno2, alumno3..) --> son los "objetos"
+y el.init los inicializa"""
+
+class Alumnos:
+    
+    def __init__(self, nombre, edad, asignatura, nota):
+        self.nombre = nombre
+        self.edad = edad
+        self.asignatura = asignatura
+        self.nota = nota
+
+
+alumno1 = Alumnos("Joao", 67, "Fisica", 8)
+alumno2 = Alumnos("Felipe", 37, "Mandarim" , 9)
+alumno3 = Alumnos("Luana", 45, "Matematicas", 10)
+alumno4 = Alumnos("Toni", 27, "Geografia", 5)
+
+"""A continuación muestra la edad del alumno 2 y el alumno 3 y sus"""
+
+print(alumno2.edad)

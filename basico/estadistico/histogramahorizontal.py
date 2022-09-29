e1009cc6009a2dfad0bfb9a4428eb653cc7af95a
newetty <ettyguerra@gmail.com>
2022-09-29 12:18:35 +0200

    terminado

---
+++ basico/estadistico/histogramahorizontal.py
@@ -0,0 +1,26 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+plt.rcdefaults()
+fig, ax = plt.subplots()
+
+# Example data
+people = [31,10,85,25,4,83,32,43,66,18,93,6,42,27,
+                         21,42,53,32,85,32,42,58,67,17,4,5]
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+ax.barh(y_pos, performance, xerr=error, align='center')
+ax.set_yticks(y_pos, labels=people)
+ax.invert_yaxis()  # labels read top-to-bottom
+ax.set_xlabel('Performance')
+ax.set_title('How fast do you want to go today?')
+
+plt.show()

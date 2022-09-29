23e8defaeac0eb7835a1094d53de2451f8b060ae
newetty <ettyguerra@gmail.com>
2022-09-29 12:18:43 +0200

    Subir

--- basico/estadistico/import pandas as pd.py
+++ basico/estadistico/import pandas as pd.py
@@ -4,11 +4,7 @@ import numpy as np
 
 # EJERCICIO 1
 
-"""Dada estos valores: 15 16 17 16 21 22 15 16 15 17 16 22 14 13 14 14 15 15 14 15 16 10 19 15 15 22 24 25 15 16#
-
-
-
-# 
+"""Dada estos valores: 15 16 17 16 21 22 15 16 15 17 16 22 14 13 14 14 15 15 14 15 16 10 19 15 15 22 24 25 15 16# 
 # 1) Realiza un Histograma con tamaño de barra 2.
 # realizar un histagrama:"""
 #opcion 1 
@@ -26,7 +22,7 @@ def histograma(df, bin):
     plt.title("Mi primer Histograma")
     plt.show()
 
-histograma(df, 2)
+#histograma(df, 2)
 # opcion 2 
 
 #y_pos = np.arange(len(df))
@@ -52,3 +48,6 @@ histograma(df, 2)
 ##histograma(df,20)
 
 #5) ¿Qué parece indicar el sesgo en la distribución?#
+
+
+

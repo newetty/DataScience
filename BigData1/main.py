9630f71815400a1bbd4f3038db24ce1e521aa26b
newetty <ettyguerra@gmail.com>
2022-10-05 17:05:54 +0200

    Ejercicio

--- BigData1/main.py
+++ BigData1/main.py
@@ -11,14 +11,26 @@ app = FastAPI()
 
 # doy la ruta donde se encuentra el dataset:
 #MEDIA_ROOT = "iris.csv"
-MEDIA_ROOT = "/home/isabel/FEI_projects/DataScience/media/iris.csv"
+# MEDIA_ROOT = "/home/isabel/FEI_projects/DataScience/media/iris.csv"
 
 #Método GET a la url "/test/"
 # llamaremos a nuestra aplicación (<app name> + <método permitido>)
 @app.get("/test/")
 async def test_1():
-    return "Bienvenido a FastAPI"
+   # return "Bienvenido a FastAPI"
 
-
-
+@app.get("/iris/")
+async def iris(response: Response):
+    try:
+        # Crear el dataframe con la información de iris:
+        df = pd.read_csv(MEDIA_ROOT)
+        # print(df)
+        # lo transformamos a json para poder gestionarlo desde el front:
+        data = df.to_json(orient="index")
+        # cargar la información con formato Json:
+        data = json.loads(data)
+        return data
+    except:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return "404 NOT FOUND"
 

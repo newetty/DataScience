f415cfc0208bb41c857e193413c57116438962bd
newetty <ettyguerra@gmail.com>
2022-10-05 19:23:28 +0200

    Seguir

--- BigData1/main.py
+++ BigData1/main.py
@@ -7,29 +7,30 @@ from pydantic import BaseModel
 
 
 # Creamos nuestra primera aplicación FastAPI:
-app = FastAPI()
+#app = FastAPI()
 
 # doy la ruta donde se encuentra el dataset:
 #MEDIA_ROOT = "iris.csv"
-# MEDIA_ROOT = "/ettyqueiroz/Documentos/Datascience/iris.csv"
+
+ MEDIA_ROOT = "/ettyqueiroz/Documentos/Datascience/iris.csv"
 
 #Método GET a la url "/test/"
 # llamaremos a nuestra aplicación (<app name> + <método permitido>)
 @app.get("/test/")
 async def test_1():
-  # return "Bienvenido a FastAPI"
+  #return "Bienvenido a FastAPI"
   # @app.get("/iris/")
   async def iris(response: Response):
     try:
-        # Crear el dataframe con la información de iris:
-        df = pd.read_csv(MEDIA_ROOT)
-        # print(df)
+        # Crear el dataframe con la información de iris
+        # df = pd.read_csv(MEDIA_ROOT)
+        #print(df)
         # lo transformamos a json para poder gestionarlo desde el front:
         data = df.to_json(orient="index")
         # cargar la información con formato Json:
         data = json.loads(data)
-        #return data
+       # return data
     except:
         response.status_code = status.HTTP_404_NOT_FOUND
-        # return "404 NOT FOUND"
+        #return "404 NOT FOUND"
 
